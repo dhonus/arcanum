c191ef754d1c50a64f4428dacefe01c1ff322270
Daniel Honus <danielhonus@tutanota.com>
2023-04-12 12:57:58 +0200

    fixed error propagation on feed pull

--- src-tauri/src/routes/parser.rs
+++ src-tauri/src/routes/parser.rs
@@ -3,35 +3,32 @@ use std::io::BufReader;
 use std::{io};
 use reqwest::Client;
 use rss::{Channel};
+use std::error::Error;
 extern crate rand;
 
-pub async fn pull(url: &str, filename: &str, client: &Client) -> Result<(), String> {
+pub async fn pull(url: &str, filename: &str, client: &Client) -> Result<(), Box<dyn Error>> {
     use rand::distributions::{Alphanumeric, DistString};
     let string = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
     let string = format!("{}?{}", url, string);
 
     let resp = client.get(string).send().await;
-    match resp {
-        Ok(resp) => {
-            let body = resp.text().await;
-            println!("Trying to remove {}", filename);
-            match std::fs::remove_file(filename){
-                Ok(_) => println!("Removed file"),
-                Err(_) => println!("File does not exist"),
-            }
-            println!("Trying to create file {}", filename);
-            let stripped = filename.strip_suffix("feed.xml").unwrap();
-            match std::fs::create_dir_all(stripped){
-                Ok(_) => println!("Created dir"),
-                Err(_) => println!("Dir already exists"),
-            }
-
-            let mut out = File::create(filename).expect("failed to create file");
-            io::copy(&mut body.unwrap().as_bytes(), &mut out).expect("failed to copy content");
-            Ok(())
-        },
-        Err(err) => Err(format!("Error in my_function: {}", err)),
+    let body = resp?.text().await;
+    println!("Trying to remove {}", filename);
+    match std::fs::remove_file(filename){
+        Ok(_) => println!("Removed file"),
+        Err(_) => println!("File does not exist"),
     }
+    println!("Trying to create file {}", filename);
+    let stripped = filename.strip_suffix("feed.xml").unwrap();
+    match std::fs::create_dir_all(stripped){
+        Ok(_) => println!("Created dir"),
+        Err(_) => println!("Dir already exists"),
+    }
+
+    let mut out = File::create(filename).expect("failed to create file");
+    io::copy(&mut body?.as_bytes(), &mut out).expect("failed to copy content");
+    
+    Ok(())
 }
 
 pub fn parse(file: String) -> Channel {
